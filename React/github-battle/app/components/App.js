bcd4c791f87e5b08b2be16d576080f9a01968a6f
Robert Adam Sedgwick <RaSedgwick@gmail.com>
2017-08-24 22:30:03 -0400

    Basic Router set up

--- React/github-battle/app/components/App.js
+++ React/github-battle/app/components/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Popular from './Popular';
+import { Nav } from './Nav';
+let ReactRouter = require('react-router-dom');
+
+
+let Router = ReactRouter.BrowserRouter;
+let Route = ReactRouter.Route;
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className='container' >
-        <Popular />
-      </div>
+      <Router>
+        <div className='container' >
+          <Nav />
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>   
     );
   }
 }
