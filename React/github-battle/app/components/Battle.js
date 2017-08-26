79a9d9826d7c86a8e1e9a9eba7898177d4570e88
Robert Adam Sedgwick <RaSedgwick@gmail.com>
2017-08-26 16:15:23 -0400

    Working on battle forms

--- React/github-battle/app/components/Battle.js
+++ React/github-battle/app/components/Battle.js
@@ -2,6 +2,28 @@ import React from 'react';
 
 
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playOneName: '',
+      playTwoName: '',
+      plyaerOneImage: null,
+      playerTwoImage: null
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit (id, username) {
+    this.setState(function () {
+      let newState = {};
+      newState[id + 'Name'] = username;
+      newState[id+ 'Image'] = 'https://github.com/' + username + '.png?size=200';
+      return newState;
+    });
+  }
+
   render () {
     return (
       <div className='home-contianer'>
@@ -10,3 +32,5 @@ export default class Battle extends React.Component {
     );
   }
 }
+
+// Left off, Forms: 10:20.
