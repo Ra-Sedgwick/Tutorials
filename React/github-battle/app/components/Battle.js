5171c515cd695241d8b983a7dd9870cf4c41b711
Robert Adam Sedgwick <RaSedgwick@gmail.com>
2017-08-27 13:15:13 -0400

    PlayerInput

--- React/github-battle/app/components/Battle.js
+++ React/github-battle/app/components/Battle.js
@@ -1,4 +1,69 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+class PlayerInput extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state ={
+      username: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    let value = event.target.value;
+
+    this.setState(function () {
+      return {
+        username: value
+      };
+    });
+  }
+
+  handleSubmit(event) {
+
+    // Prevent submitting to server
+    event.preventDefault();
+    this.props.onSubmit(
+      this.props.id,
+      this.state.username
+    );
+  }
+
+  render() {
+    return (
+      <form className='column' onSubmit={this.handleSubmit}>
+        <label className='header' htmlFor='username'>
+          {this.props.label}
+        </label>
+        <input 
+          id='username'
+          placeholder='github username'
+          type='text'
+          autoComplete='off'
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <button
+          className='button'
+          type='submit'
+          disabled={!this.state.username}
+        >
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+PlayerInput.propTypes ={
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired
+}
 
 
 export default class Battle extends React.Component {
@@ -25,12 +90,27 @@ export default class Battle extends React.Component {
   }
 
   render () {
+
+    let playerOneName = this.state.playerOneName;
+    let playerTwoName = this.state.playerTwoName;
+
     return (
-      <div className='home-contianer'>
-        <h1>Battle!</h1>
+      <div>
+        <div className='row'>
+          {!playerOneName && 
+            <PlayerInput 
+              id='playerOne' 
+              label='Player One' 
+              onSubmit={this.handleSubmit}
+            />}
+          {!playerTwoName && 
+            <PlayerInput 
+              id='playerTwo'
+              label='Player Two'
+              onSubmit={this.handleSubmit}
+            />}
+        </div>
       </div>
     );
   }
 }
-
-// Left off, Forms: 10:20.
