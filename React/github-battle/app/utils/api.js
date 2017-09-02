cc87e45558e4721d6c35df51e38f14e50dbae0c7
Robert Adam Sedgwick <RaSedgwick@gmail.com>
2017-09-01 23:21:22 -0400

    Aixos, Promises...

--- React/github-battle/app/utils/api.js
+++ React/github-battle/app/utils/api.js
@@ -1,5 +1,69 @@
 import axios from 'axios';
 
+// const id = 'YOUR_CLIENT_ID';
+// const sec = 'YOU_SECRET';
+// const params = '?clien_id=' + id + '&client_secret=' + sec;
+
+function getProfile (username) {
+  // Axios returns Promises with an obj with .then prop
+  // User is what we got back form the git hub api
+  // It is only invoked when the get is resolved
+  return axios.get('https://api.github.com/users/' + username)
+    .then(function (user) {
+      return user.data;
+    });
+}
+
+function getRepos (username) {
+  axios.get('https://api.github.com/users/' + username + '/repos&per_page=100');
+}
+
+function getStarCount (repos) {
+  return repos.data.reduce(function (count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore (profile, repos) {
+  let followers = profile.followers;
+  let totalStars = getStarCount(repos);
+  return (followers * 3 + totalStars);
+}
+
+function handleError (error) {
+  console.warn(error);
+  return null;
+}
+
+// Axios.all takes an array of promises and resolves 
+// Once then have all returned, returns an array of json objects
+function getUserData (player) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(function (data) {
+    let profile = data[0];
+    let repos = data[1];
+
+    return {
+      profile: profile,
+      score: calculateScore(profile, repos)
+    };
+  });
+}
+
+function sortPlayers (players) {
+  return players.sort(function (a, b) {
+    return b.score - a.score;
+  });
+}
+
+export function battle (players) {
+  return axios.all(players.map(getUserData))
+    .then(sortPlayers)
+    .catch(handleError);
+}
+
 export function fetchPopularRepos(language) {
 
   var encodedURI = window.encodeURI(
