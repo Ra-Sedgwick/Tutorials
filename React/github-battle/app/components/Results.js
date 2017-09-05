c8f18d3a526a1e5799996bdc5a1c422c3e1400a5
Robert Adam Sedgwick <RaSedgwick@gmail.com>
2017-09-04 22:32:53 -0400

    baisc components complete

--- React/github-battle/app/components/Results.js
+++ React/github-battle/app/components/Results.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import queryString from 'query-string';
-import Link from 'react-router-dom';
-import PlayerPreview from './PlayerPreview';
+import { Link } from 'react-router-dom';
+import { PlayerPreview } from './PlayerPreview';
 let api = require('../utils/api');
 
+
+function Profile (props) {
+
+  let info = props.info;
+
+  return (
+    <PlayerPreview
+      avatar={info.avatar_url}
+      username={info.login}
+    >
+      <ul className='space-list-items'>
+        {info.name     && <li>{info.name}</li>}
+        {info.location && <li>{info.location}</li>}
+        {info.company  && <li>{info.company}</li>}
+        <li>Followers: {info.followers}</li>
+        <li>Following: {info.following}</li>
+        <li>Public Repos: {info.public_repos}</li>
+        {info.blog && <li><a href={info.blog}></a></li>}
+      </ul>
+
+    </PlayerPreview>
+  );
+}
+
+Profile.propTypes = {
+  info: PropTypes.object.isRequired
+};
 
 function Player (props) {
   return (
     <div>
       <h1 className='header'>{props.label}</h1>
       <h3 style={{textAlign: 'center'}}>Score: {props.score}</h3>
+      <Profile 
+        info={props.profile}
+      />
     </div>
   );
 }
