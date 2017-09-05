7dc5191284fb6016a09545fb7b39c5ba30cd45a2
Robert Adam Sedgwick <RaSedgwick@gmail.com>
2017-09-04 21:39:35 -0400

    Fixed api

--- React/github-battle/app/components/Results.js
+++ React/github-battle/app/components/Results.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import queryString from 'query-string';
+let api = require('../utils/api');
 
 export default class Result extends React.Component {
+  componentDidMount() {
+    let players = queryString.parse(this.props.location.search);
+
+    api.battle([
+      players.playerOneName,
+      players.playerTwoName
+    ]).then(function (results) {
+      console.log(results);
+    });
+  }
   render() {
-    console.log(this.props);
+    
     return (
       <div>
         Results
